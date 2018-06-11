613f5ad3d3fd7fe853ef3874951de8bc95d19486
Emmanuel Olivo <emmanuelo1002@gmail.com>
2018-06-11 14:38:49 -0500

    Add navbar with links

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,11 +1,12 @@
 import React, {Component} from 'react';
 // import axios from '../../axios';
-import {Route} from 'react-router-dom';
+import {Route, NavLink} from 'react-router-dom';
 
 
 import './Blog.css';
 
 import Posts from './Posts/Posts';
+import NewPost from './NewPost/NewPost';
 
 class Blog extends Component {
   render() {
@@ -14,12 +15,16 @@ class Blog extends Component {
         <header>
           <nav>
             <ul>
-              <li><a href="/">Home</a></li>
-              <li><a href="/new-post">New Post</a></li>
+              <li><NavLink to="/" exact>Home</NavLink></li>
+              <li><NavLink to={{
+                pathname: "/new-post",
+                hash: '#submit'
+              }}>New Post</NavLink></li>
             </ul>
           </nav>
         </header>
         <Route path="/" exact component={Posts}/>
+        <Route path="/new-post" component={NewPost}/>
       </div>
     );
   }
